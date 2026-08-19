7cbe516d11764a416d1b6edf7f877e88b37785af
Martin Hooper <martin@mjhooper.co.uk>
2026-08-19 14:25:50 +0100

    remove wezterm config

--- .wezterm.lua
+++
@@ -1,9 +0,0 @@
--- Pull in the wezterm API
-local wezterm = require("wezterm")
-
-local config = wezterm.config_builder()
-
-config.font = wezterm.font("Ubuntu Mono")
-config.font_size = 19
-
-return config
